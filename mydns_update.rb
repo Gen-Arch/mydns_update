f6400ae60a23bac7c0f25dc505c1cc0deddef706
Gen-Arch <ymdtshm@gmail.com>
2019-01-11 22:23:14 +0900

    update

--- mydns_update.rb
+++ mydns_update.rb
@@ -7,19 +7,19 @@ require 'yaml'
 class Mydns
 
   def update
-    data = YAML.load_file("config.yml")
+    data = YAML.load_file(File.join(__dir__, "config.yml"))
     res = http_request(data)
     put_log(res)
   end
 
   private
   def http_request(data)
-    uri = URI.parse("https://www.mydns.jp/directip.html")
+    uri = URI.parse("https://ipv4.mydns.jp/login.html")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req = Net::HTTP::Post.new(uri.path)
-    req.set_form_data(data)
+    req = Net::HTTP::Get.new(uri.path)
+    req.basic_auth data["MID"], data["PWD"]
     http.request(req)
   end
 
